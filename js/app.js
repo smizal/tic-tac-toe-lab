b0fa85ae2db14ceeb1e9ee68aa0d73e65ec9f81d
smizal <sjs29@hotmail.com>
2024-09-29 20:36:43 +0300

    Tic-Tac-Toe Game Lab

--- js/app.js
+++ js/app.js
@@ -16,15 +16,60 @@ let turn
 let winner
 let tie
 let winnerCombo
+let playerOneName
+let playerTwoName
+let playerOneScore
+let playerTwoScore
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.querySelector('#message')
-const resetBtnEl = document.querySelector('#reset')
+const playerOneCounterEl = document.querySelector('#playerOneCounter')
+const playerTwoCounterEl = document.querySelector('#playerTwoCounter')
+const resetGameBtnEl = document.querySelector('#resetGame')
+const resetTurnBtnEl = document.querySelector('#resetTurn')
 
 /*-------------------------------- Functions --------------------------------*/
 
-const init = () => {
+const initGame = () => {
+  playerOneName = document.querySelector('#playerOneName').value
+  if (playerOneName.trim() === '') {
+    messageEl.textContent = `Error: Game is not initialized. Please enter player one name`
+    messageEl.classList.add('error')
+    return
+  }
+  playerTwoName = document.querySelector('#playerTwoName').value
+  if (playerTwoName.trim() === '') {
+    messageEl.textContent = `Error: Game is not initialized. Please enter player Two name`
+    messageEl.classList.add('error')
+    return
+  }
+
+  squareEls.forEach((squareEl) => {
+    squareEl.classList.remove('winner')
+  })
+  document.querySelectorAll('.hidden').forEach((hiddenEl) => {
+    hiddenEl.classList.remove('hidden')
+  })
+  playerOneCounterEl.textContent = ''
+  playerTwoCounterEl.textContent = ''
+  messageEl.classList.remove('winner')
+  messageEl.classList.remove('error')
+  board = ['', '', '', '', '', '', '', '', '']
+  turn = 'X'
+  winner = false
+  tie = false
+  winnerCombo = []
+  playerOneScore = 0
+  playerTwoScore = 0
+  /* localStorage.setItem('playerOneName', playerOneName)
+  localStorage.setItem('playerOneScore', playerOneScore)
+  localStorage.setItem('playerTwoName', playerTwoName)
+  localStorage.setItem('playerTwoScore', playerTwoScore) */
+  render()
+}
+
+const initTurn = () => {
   squareEls.forEach((squareEl) => {
     squareEl.classList.remove('winner')
   })
@@ -50,22 +95,43 @@ const updateBoard = () => {
 
 const updateMessage = () => {
   if (!winner && !tie) {
-    messageEl.textContent = `It is ${turn} turn now` // render whose turn it is.
+    if (turn === 'X') {
+      messageEl.textContent = `It is ${playerOneName} (${turn}) turn now` // render whose turn it is.
+    } else {
+      messageEl.textContent = `It is ${playerTwoName} (${turn}) turn now` // render whose turn it is.
+    }
   } else if (!winner && tie) {
     messageEl.textContent = `The game finished, no winner in this turn. good luck next time.` // render a tie message.
   } else {
     styleWinnerSquares()
-    messageEl.textContent = `Congratulation ${turn}, You won this turn` // render a congratulatory message to the player that has won
+    if (turn === 'X') {
+      messageEl.textContent = `Congratulation ${playerOneName} (${turn}), You won this turn` // render a congratulatory message to the player that has won
+    } else {
+      messageEl.textContent = `Congratulation ${playerTwoName} (${turn}), You won this turn` // render a congratulatory message to the player that has won
+    }
+
     messageEl.classList.add('winner')
+    updateMessageCounter()
   }
 }
 
+const updateMessageCounter = () => {
+  if (turn === 'X') {
+    playerOneScore += 1
+  } else {
+    playerTwoScore += 1
+  }
+  playerOneCounterEl.textContent = `player (x): ${playerOneName} Score: ${playerOneScore}`
+  playerTwoCounterEl.textContent = `player (O): ${playerTwoName} Score: ${playerTwoScore}`
+}
 const handleClick = (event) => {
   let squareIndex = event.target.id
+  console.log(squareIndex)
   if (board[squareIndex] !== '' || winner) {
     return
   } else {
     placePiece(squareIndex)
+    console.log(board)
     checkForWinner()
     checkForTie()
     switchPlayerTurn()
@@ -134,6 +200,7 @@ squareEls.forEach((squareEl) => {
   squareEl.addEventListener('click', handleClick)
 })
 
-resetBtnEl.addEventListener('click', init)
+resetGameBtnEl.addEventListener('click', initGame)
+resetTurnBtnEl.addEventListener('click', initTurn)
 
-init()
+// initGame()
